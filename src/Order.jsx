2aa45fc4c2d38d8bb5a3f233805a3091cc143d82
Rampranav Vadlamani <rampranavccp@gmail.com>
2025-10-29 12:31:11 +0530

    fix:pizza component

--- src/Order.jsx
+++ src/Order.jsx
@@ -7,97 +7,99 @@ const intl = new Intl.NumberFormat("en-IN", {
 });
 
 function Order() {
-    const [pizzaType, setPizzaType] = useState("Pepperoni");
+    const [pizzaType, setPizzaType] = useState("bbq_ckn");
     const [pizzaSize, setPizzaSize] = useState("L");
     const [pizzas, setPizzas] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    let price,selectedPizza;
+    let price, selectedPizza;
 
-    if(!loading){
-        selectedPizza = pizzas.find(pizza=>pizza.id===pizzaType);
+
+
+    if (!loading) {
+        selectedPizza = pizzas.find(pizza => pizza.id === pizzaType);
+        console.log(selectedPizza);
         // price = selectedPizza[pizzaSize];
     }
 
-    console.log(pizzas);
-
-    async function getData(){
+    async function getData() {
+        setLoading(true);
+        await new Promise(resolve => setTimeout(resolve, 1000));
         const response = await fetch(`/api/pizzas/`);
         const data = await response.json();
         setPizzas(data);
+        setLoading(false);
     }
 
-    useEffect(()=>{
-        setLoading(true);
+    useEffect(() => {
         getData();
     }, []);
 
-    useEffect(()=>{
-        setLoading(false);
-    }, [pizzas]);
 
     return (
-        <div className="container mx-auto">
-            <h1 className="text-2xl font-bold text-center">Your Order</h1>
-            <form className="flex flex-col items-center">
-                <div className="flex flex-col items-center justify-center gap-4">
-                    <div className="flex flex-col items-center my-4">
-                        <label htmlFor="pizza-type" className="text-lg font-bold">Pizza Type</label>
-                        <select onChange={(e)=>setPizzaType(e.target.value)} id="pizza-type" value={pizzaType} className="w-full p-2 rounded-md cursor-pointer ring-1 ring-gray-300 px-10 mt-4">
-                            {pizzas.map(obj=><option key={obj.id} value={obj.id}>{obj.name}</option>)}
-                        </select>
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <label htmlFor="pizza-size" className="text-lg font-bold">Pizza Size</label>
-                        <div className="flex flex-row items-center justify-center gap-4">
-                            <span className="flex flex-row items-center justify-center gap-3">
-                                <input type="radio"
-                                    checked={pizzaSize === "S"}
-                                    name="pizza-size"
-                                    id="pizza-s"
-                                    value="S"
-                                    onChange={(e)=>setPizzaSize(e.target.value)}
-                                />
-                                <label htmlFor="pizza-s" className="text-lg">Small</label>
-                            </span>
-                            <span className="flex flex-row items-center justify-center gap-3">
-                                <input type="radio"
-                                    checked={pizzaSize === "M"}
-                                    name="pizza-size"
-                                    id="pizza-m"
-                                    value="M"
-                                    onChange={(e)=>setPizzaSize(e.target.value)}
-                                />
-                                <label htmlFor="pizza-s" className="text-lg">Medium</label>
-                            </span>
-                            <span className="flex flex-row items-center justify-center gap-3">
-                                <input type="radio"
-                                    checked={pizzaSize === "L"}
-                                    name="pizza-size"
-                                    id="pizza-l"
-                                    value="L"
-                                    onChange={(e)=>setPizzaSize(e.target.value)}
-                                />
-                                <label htmlFor="pizza-s" className="text-lg">Large</label>
-                            </span>
+        loading ? <div className="flex items-center justify-center h-screen">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div> :  <div className="container mx-auto">
+                    <h1 className="text-2xl font-bold text-center">Your Order</h1>
+                    <form className="flex flex-col items-center">
+                        <div className="flex flex-col items-center justify-center gap-4">
+                            <div className="flex flex-col items-center my-4">
+                                <label htmlFor="pizza-type" className="text-lg font-bold">Pizza Type</label>
+                                <select onChange={(e) => setPizzaType(e.target.value)} id="pizza-type" value={pizzaType} className="w-full p-2 rounded-md cursor-pointer ring-1 ring-gray-300 px-10 mt-4">
+                                    {pizzas.map(obj => <option key={obj.id} value={obj.id}>{obj.name}</option>)}
+                                </select>
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <label htmlFor="pizza-size" className="text-lg font-bold">Pizza Size</label>
+                                <div className="flex flex-row items-center justify-center gap-4">
+                                    <span className="flex flex-row items-center justify-center gap-3">
+                                        <input type="radio"
+                                            checked={pizzaSize === "S"}
+                                            name="pizza-size"
+                                            id="pizza-s"
+                                            value="S"
+                                            onChange={(e) => setPizzaSize(e.target.value)}
+                                        />
+                                        <label htmlFor="pizza-s" className="text-lg">Small</label>
+                                    </span>
+                                    <span className="flex flex-row items-center justify-center gap-3">
+                                        <input type="radio"
+                                            checked={pizzaSize === "M"}
+                                            name="pizza-size"
+                                            id="pizza-m"
+                                            value="M"
+                                            onChange={(e) => setPizzaSize(e.target.value)}
+                                        />
+                                        <label htmlFor="pizza-s" className="text-lg">Medium</label>
+                                    </span>
+                                    <span className="flex flex-row items-center justify-center gap-3">
+                                        <input type="radio"
+                                            checked={pizzaSize === "L"}
+                                            name="pizza-size"
+                                            id="pizza-l"
+                                            value="L"
+                                            onChange={(e) => setPizzaSize(e.target.value)}
+                                        />
+                                        <label htmlFor="pizza-s" className="text-lg">Large</label>
+                                    </span>
+                                </div>
+                            </div>
+                            <button type="submit" className="bg-blue-500 text-white p-2 rounded-md cursor-pointer ">Order Pizza</button>
                         </div>
-                    </div>
-                    <button type="submit" className="bg-blue-500 text-white p-2 rounded-md cursor-pointer ">Order Pizza</button>
+                        <div className="flex flex-row items-center justify-center gap-4 my-4">
+                            <div className="flex flex-col items-center justify-center gap-4 bg-gray-100 p-4 rounded-md">
+                                <Pizza
+                                    name={selectedPizza?.name}
+                                    image={selectedPizza?.image}
+                                    description={selectedPizza?.description}
+                                    price={selectedPizza?.sizes[pizzaSize] * 50}
+                                />
+                                <p className="text-lg font-bold text-center">Pizza Size: {pizzaSize === "S" ? "Small" : pizzaSize === "M" ? "Medium" : "Large"}</p>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div className="flex flex-row items-center justify-center gap-4 my-4">
-                    <div className="flex flex-col items-center justify-center gap-4 bg-gray-100 p-4 rounded-md">
-                        <Pizza
-                            name={selectedPizza?.name}
-                            image={`/public/pizzas/${selectedPizza?.id}.webp`}
-                            description="Mozzarella Cheese, Pepperoni"
-                        />
-                        <p className="text-lg font-bold text-center">Pizza Size: {pizzaSize==="S" ? "Small" : pizzaSize==="M" ? "Medium" : "Large"}</p>
-                        <p className="text-lg font-bold text-center">Total: {selectedPizza && intl.format(selectedPizza.sizes[pizzaSize]*50)}</p>
-                    </div>
-                </div>
-            </form>
-        </div>
-    );
+        )
 }
 
 export default Order;
--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,13 +1,14 @@
 function Pizza({ name, image, price, description }) {
   return (
-    <div onClick={()=>{}} className="bg-white rounded-lg m-4 shadow-md p-4 max-w-xs mx-auto flex w-xl flex-col items-center">
+    <div className="bg-white rounded-lg m-4 shadow-md p-4 max-w-xs mx-auto flex w-xl flex-col items-center">
       <img
         src={image}
         alt={name}
         className="w-32 h-32 object-cover rounded-full mb-4"
       />
       <h2 className="text-xl font-semibold mb-2">{name} </h2>
-      <p className="text-gray-600 text-sm mb-2">{description}</p>
+      <p className="text-gray-600 text-sm mb-2 line-clamp-2 text-ellipsis overflow-hidden">{description}</p>
+      <p className="text-gray-600 text-sm mb-2 font-bold">Price: {price}</p>
     </div>
   );
 }
